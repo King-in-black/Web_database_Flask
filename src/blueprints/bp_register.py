29a8a9374efc51237c71efeca55f6721754edcec
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-03-01 16:38:23 +0000

    Finish the adjustment and start to write the workflow in github

--- src/blueprints/bp_register.py
+++ src/blueprints/bp_register.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, jsonify
 
-register_bp = Blueprint("get", __name__)
+register_bp = Blueprint("register", __name__)
 from ..models import Player, Trainer, Data
 from ..schemas import Player_Schema, Trainer_Schema, Data_Schema
 from ..extension import db
-
 
 
 
