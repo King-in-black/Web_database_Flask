f8c561e493d43230a3cdb76022563961afd9a781
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-04-02 15:03:31 +0100

    Finish Debug of tests configuration; Start to program formal test functions

--- src/tests/test_rest.py
+++ src/tests/test_rest.py
@@ -1,5 +1,20 @@
-import pytest
 
 def test_home_page(driver, live_server):
+    '''
+    A trainer wants to register an account to predict the result of activity for IMU
+    1. He needs to jump in Homepage
+    2. He needs to jump to register page
+    3. He needs to choose role as trainer and type the user_name as arnold, password as 123456
+    4. He registers successfully and jumps to login page
+    5. He logins in with the password and jumps to predict page
+    6. He types in 0.1,0.1,0.1,0.1,0.1,0.1 for AccX, AccY, AccZ, gyroX, gyroY and gyroZ.
+    7. The results should be:
+    Movement: stationary
+    Prediction: 0.0
+
+    :param driver: the driver to Google Chrome
+    :param live_server: the live_server on the Selenium tests
+    :return:
+    '''
     driver.get("http://localhost:5000")
-    assert "Hello, Flask!" in driver.page_source
+    assert "homepage" in driver.page_source
