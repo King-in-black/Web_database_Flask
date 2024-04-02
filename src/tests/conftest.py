aa3745e46bee4e5d573d8000bde685f749da46d1
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-04-02 15:43:51 +0100

    Set the user stories and test functions.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -7,6 +7,7 @@ import socket
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 option = webdriver.ChromeOptions()
 option.add_argument("start-maximized")
 
--- src/tests/test_rest.py
+++ src/tests/test_rest.py
@@ -1,12 +1,14 @@
+import time
 
+from selenium.webdriver.common.by import By
 def test_complete_function_1(driver, live_server):
     '''
     A trainer wants to register an account to predict the result of activity for IMU
-    1. He needs to jump in Homepage through the button in card body
-    2. He needs to jump to register page through the navigation bar
+    1. He needs to jump in register through the button in card body
+    2. He needs to jump to login page through the navigation bar
     3. He needs to choose role as trainer and type the user_name as arnold, password as 123456
     4. He registers successfully and jumps to login page
-    5. He login in with the password and jumps to predict page
+    5. He logs in  with the password and jumps to predict page
     6. He types in 0.1,0.1,0.1,0.1,0.1,0.1 for AccX, AccY, AccZ, gyroX, gyroY and gyroZ.
     7. The results should be:
     Movement: stationary
@@ -19,14 +21,25 @@ def test_complete_function_1(driver, live_server):
     '''
     driver.get("http://localhost:5000")
     assert "homepage" in driver.page_source
+    time.sleep(2)
+    # find register link through navigation bar
+    register_link = driver.find_element(By.LINK_TEXT, "register")
+    register_link.click()
+    assert driver.current_url.endswith("/register")
+    register_form_player_ID = driver.find_element(By.ID, "register")
+    register_form_trainer_ID = driver.find_element(By.ID, "register")
+    register_option_role = driver.find_element(By.ID, "register")
+    register_form_password = driver.find_element(By.ID, "register")
+
+
 def test_complete_function_2(driver, live_server):
     '''
     A player wants to register an account to predict the result of activity for IMU
-    1. He needs to jump in Homepage through the button in card body
-    2. He needs to jump to register page through the page button
-    3. He needs to choose role as player and type the user_name as a, password as 123456
-    4. He finds there is an account_ID in a already
-    5. He login in with account name 'a' and password 'a' and jumps to predict page
+    1. He needs to jump in register through the button in card body
+    2. He needs to jump to login page through the page button
+    3. He needs to choose role as player and type the user_name as a password as 123456
+    4. He finds there is an account_ID already
+    5. He logs in with account name 'a' and password 'a' and jumps to predict page
     6. He types in 1,1,1,1,1,1 for AccX, AccY, AccZ, gyroX, gyroY and gyroZ.
     7. The results should be:
     Movement: Moving
