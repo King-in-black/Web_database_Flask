26c879950439c5d4bf073acb2bdae080faad2ae9
Pikaqu_tesla <arnoldzhang_inf@163.com>
2024-04-02 16:03:19 +0100

    Constructing a user story test

--- src/tests/test_rest.py
+++ src/tests/test_rest.py
@@ -1,12 +1,12 @@
 import time
-
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 def test_complete_function_1(driver, live_server):
     '''
     A trainer wants to register an account to predict the result of activity for IMU
     1. He needs to jump in register through the button in card body
-    2. He needs to jump to login page through the navigation bar
-    3. He needs to choose role as trainer and type the user_name as arnold, password as 123456
+    2. He needs to choose role as trainer and type the user_name as arnold, password as 123456
+    3. He needs to jump to login page through the navigation bar
     4. He registers successfully and jumps to login page
     5. He logs in  with the password and jumps to predict page
     6. He types in 0.1,0.1,0.1,0.1,0.1,0.1 for AccX, AccY, AccZ, gyroX, gyroY and gyroZ.
@@ -20,17 +20,28 @@ def test_complete_function_1(driver, live_server):
     :return:
     '''
     driver.get("http://localhost:5000")
+    # Here is the home page
     assert "homepage" in driver.page_source
     time.sleep(2)
     # find register link through navigation bar
+    # step 1:
     register_link = driver.find_element(By.LINK_TEXT, "register")
     register_link.click()
     assert driver.current_url.endswith("/register")
-    register_form_player_ID = driver.find_element(By.ID, "register")
-    register_form_trainer_ID = driver.find_element(By.ID, "register")
-    register_option_role = driver.find_element(By.ID, "register")
-    register_form_password = driver.find_element(By.ID, "register")
-
+    # step 2:
+    register_form_player_ID = driver.find_element(By.ID, "Player_ID")
+    register_form_trainer_ID = driver.find_element(By.ID, "Trainer_ID")
+    register_option_role = driver.find_element(By.ID, "Role")
+    register_form_password = driver.find_element(By.ID, "password")
+    register_form_player_ID.send_keys("a")
+    register_form_trainer_ID.send_keys("arnold")
+    register_form_password.send_keys("123456")
+    role_select = Select(register_option_role)
+    role_select.select_by_visible_text("trainer")
+    button_register = driver.find_element(By.ID, "submit")
+    button_register.click()
+    assert driver.current_url.endswith("/login")
+    # step 3:
 
 def test_complete_function_2(driver, live_server):
     '''
